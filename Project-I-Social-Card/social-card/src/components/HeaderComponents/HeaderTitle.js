1498974cbd7b94c489b86882e4b6fb021b82ec1a
Robert Driskell <driskell.ba@gmail.com>
2019-04-22 16:20:00 -0500

    Took a while to get sass up and running.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,20 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
+
+const HeaderTitle = () => {
+    return (
+        <div className="title-wrapper">
+            <div className="author">
+                <h4>Lambda School</h4>
+            </div>
+            <div className="twitter">
+                <p>@LambdaSchool</p>
+            </div>
+            <div className="date">
+                <p>・26 jan</p>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderTitle;
