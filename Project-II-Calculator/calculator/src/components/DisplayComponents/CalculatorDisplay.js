3dea3331d41e6191d8d493469c8be15f49baa55b
Robert Driskell <driskell.ba@gmail.com>
2019-04-23 15:28:07 -0500

    Blocked out basic components for buttons, started styling

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
-import './Display.css';
+import './Display.scss';
+
+const ActionButton = props => {
+    return (
+        <div className="display">
+            {props.text}
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
